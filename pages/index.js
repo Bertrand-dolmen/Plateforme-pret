44d48ff08af5a288c6ed602cdb93bba1c32263cb
Bertrand-dolmen <servicefinancie0@gmail.com>
2025-04-06 13:12:19 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenue sur Prêt d'Argent</h1>
+      <p>Votre plateforme d'annonces de prêt entre particuliers</p>
+    </div>
+  );
+}
